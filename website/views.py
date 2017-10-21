4ad4b100d4f866c2462d81a8fbc874b1e0645b28
tsitsi <tsitsimunikwa97@gmail.com>
2017-10-21 12:10:43 +0200

    index for portal

--- website/views.py
+++ website/views.py
@@ -1,7 +1,57 @@
-from flask import render_template
+from flask import render_template,  flash, redirect, url_for, session,request, logging
+from passlib.hash import sha256_crypt
 
 from website import app
+from forms import RegisterForm
+
+#from website.database import db_session
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+#routing the register request
+@app.route('/Register', methods =['GET','POST'])
+def Register():
+    form = Register_Form(request.form)
+    
+    if request.method == 'POST' and form.validate():
+        
+        name = form.name.data
+        email = form.email.data
+        username = form.username.data
+        phone_number = form.phonenumber.data
+        twitter_handle = form.twitter.data
+        password = sha256_crypt.encrypt(str(form.password.data))
+        
+        return redirect(url_for('index'))
+
+
+    return render_template('Register.html', form=form)
+
+@app.route('/Login', methods = ['GET','POST'])
+def login():
+    form = LoginForm(request.form)
+
+    if request.method == 'GET' and form.validate():
+
+        username = form.username.data
+        password = sha256_crypt.decrypt(str(form.password.data))
+
+        return redirect(url_for('index'))
+    return render_template('login.html', form = form)
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+@app.route('/addpost', methods = ['POST'])
+def addpost():
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    content = request.form['content']
+
+    return '<h1>Title: {}  Subtitle: {} Author: {} Content: {}</h1>'.format(title, subtitle, author, content)
