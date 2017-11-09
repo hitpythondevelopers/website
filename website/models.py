efc7c8bb28c481883562b0816583727ff24dbfcd
Achim Munene <achimmunene@Achims-MacBook-Pro.local>
2017-11-10 00:16:41 +0200

    added some stuff

--- website/models.py
+++ website/models.py
@@ -1,31 +1,6 @@
 from datetime import date
 
-from flask import Flask, request, redirect, url_for
 from flask_login import UserMixin
 from werkzeug.utils import secure_filename
 from werkzeug.security import check_password_hash, generate_password_hash
-from website import db, app
-from config import UPLOAD_FOLDER_LOGO, ALLOWED_EXTENSIONS
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+from website import db
